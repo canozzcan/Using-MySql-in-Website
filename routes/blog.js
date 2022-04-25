9f155b61acd88828f1fec810786176953674c8f8
canozzcan <canozcanstu@gmail.com>
2022-04-25 14:17:58 +0300

    Injecting Dynamic Data - Fetch & Display Blog Post

--- routes/blog.js
+++ routes/blog.js
@@ -4,17 +4,52 @@ const db = require('../data/database');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.redirect('/posts');
+router.get('/', function (req, res) {
+  res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-    res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+  res.render('posts-list', { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-    const [authors] = await db.query('SELECT * FROM authors');
-    res.render('create-post', {authors: authors});
+router.get('/new-post', async function (req, res) {
+  const [authors] = await db.query('SELECT * FROM authors');
+  res.render('create-post', { authors: authors });
 });
+
+router.post('/posts', async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query(
+    'INSERT INTO posts (title, summary, body, author_id) VALUES (?)',
+    [data]
+  );
+  res.redirect('/posts');
+});
+
+router.get('/posts/:id', async function (req, res) {
+    const query = `
+      SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+      INNER JOIN authors ON posts.author_id = authors.id
+      WHERE posts.id = ?
+    `;
+  
+    const [posts] = await db.query(query, [req.params.id]);
+  
+    if (!posts || posts.length === 0) {
+      return res.status(404).render('404');
+    }
+  
+    res.render('post-detail', { post: posts[0] });
+  });
 
 module.exports = router;
